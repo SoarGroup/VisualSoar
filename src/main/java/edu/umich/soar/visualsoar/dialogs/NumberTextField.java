3fa580fbc6969a2a64d9991c5c716c7d30ed08ae
Andrew Nuxoll <nuxoll@up.edu>
2022-12-16 10:42:54 -0800

    Added NumberTextField.java
    I'm not sure sure how, but this file wasn't in the git repository.
    
    I've also updated .gitignore to ignore binary files generated by ant.

---
+++ src/main/java/edu/umich/soar/visualsoar/dialogs/NumberTextField.java
@@ -0,0 +1,50 @@
+package edu.umich.soar.visualsoar.dialogs;
+
+import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+/**
+ * class NumberTextField
+ *
+ * is a JTextField that only allows whole numbers
+ *
+ * Initial code was borrowed from the UpperCaseDocument example on this page:
+ * https://docs.oracle.com/javase/7/docs/api/javax/swing/JTextField.html
+ */
+public class NumberTextField extends JTextField {
+
+    public NumberTextField() {
+        super("", 3);  //set's field width
+    }
+
+    protected Document createDefaultModel() {
+        return new NumberDocument();
+    }
+
+    static class NumberDocument extends PlainDocument {
+
+        public void insertString(int offset, String str, AttributeSet a)
+                throws BadLocationException {
+
+            if (str == null) return;  //can this even happen?
+
+            //Construct the new String value the user intends
+            StringBuilder sb = new StringBuilder();
+            sb.append(this.getText(0, this.getLength()));
+            sb.insert(offset, str);
+
+            //Is this a number?
+            int numVal;
+            try {
+                numVal = Integer.parseInt(sb.toString());
+            } catch (Exception e) {
+                return;  //ignore the user's input
+            }
+
+            super.insertString(offset, str, a);
+        }
+    }
+}//class NumberTextField
