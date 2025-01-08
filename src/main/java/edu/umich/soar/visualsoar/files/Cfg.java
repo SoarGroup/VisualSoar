64b9e31fbe5ac75becd3c7872a8577a8eb23abe9
Nathan Glenn <garfieldnate@gmail.com>
2025-01-08 17:46:42 -0600

    Make cfg file read/write more robust
    * Use new atomic save utility to write the cfg file
    * Write relative paths to the cfg file so that simply opening VisualSoar on a
    different computer does not lead to cfg file changes
    * Standardize on / for path separator (which Windows understands) and \n as line
    separator so that paths are readable and the same on all platforms

--- src/main/java/edu/umich/soar/visualsoar/files/Cfg.java
+++ src/main/java/edu/umich/soar/visualsoar/files/Cfg.java
@@ -1,43 +1,46 @@
 package edu.umich.soar.visualsoar.files;
 
-import edu.umich.soar.visualsoar.mainframe.MainFrame;
+import static edu.umich.soar.visualsoar.files.Util.saveToFile;
+
 import edu.umich.soar.visualsoar.datamap.DataMap;
+import edu.umich.soar.visualsoar.mainframe.MainFrame;
 import edu.umich.soar.visualsoar.operatorwindow.OperatorNode;
 import edu.umich.soar.visualsoar.operatorwindow.OperatorRootNode;
 import edu.umich.soar.visualsoar.operatorwindow.OperatorWindow;
 import edu.umich.soar.visualsoar.operatorwindow.SoarOperatorNode;
 import edu.umich.soar.visualsoar.ruleeditor.RuleEditor;
-
-import javax.swing.*;
-import javax.swing.tree.TreeNode;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.Scanner;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.swing.*;
+import javax.swing.tree.TreeNode;
 
 public class Cfg {
+  private static final Logger LOGGER = Logger.getLogger(Cfg.class.getName());
   /**
    * readCfgFile
    *
-   * <p>is called when the project is loaded to read information about what files are open last time
+   * <p>is called when the project is loaded to read information about what files were open last time
    * and restore them
    */
   public static void readCfgFile(MainFrame mainFrame) {
     OperatorWindow operatorWindow = mainFrame.getOperatorWindow();
     // Read file contents
-    File cfgFile = getCfgFile(operatorWindow);
-    if ((cfgFile == null) || (!cfgFile.exists())) return; // nothing to do
-    Scanner scan;
-    ArrayList<String> cfgLines = new ArrayList<>();
+    Path cfgFile = getCfgFile(operatorWindow);
+    if ((cfgFile == null) || (!Files.exists(cfgFile))) {
+      return; // nothing to do
+    }
+    Path projectRoot = cfgFile.getParent();
+    List<String> cfgLines;
     try {
-      scan = new Scanner(cfgFile);
-      while (scan.hasNextLine()) cfgLines.add(scan.nextLine());
-      scan.close();
-    } catch (FileNotFoundException fnfe) {
-      mainFrame.getFeedbackManager().setStatusBarMsg("Unable to read previous configuration from " + cfgFile.getName());
+      cfgLines = Files.readAllLines(cfgFile);
+    } catch (IOException e) {
+      mainFrame.getFeedbackManager().setStatusBarMsg("Unable to read previous configuration from " + cfgFile.getFileName());
       return;
     }
 
@@ -49,13 +52,14 @@ public class Cfg {
       // Calculate the canonical path of the filename for equality comparison
       String target;
       try {
-        target = (new File(tokens[1])).getCanonicalPath();
+        target = projectRoot.resolve(Paths.get(tokens[1])).toRealPath().toString();
       } catch (IOException ioe) {
         continue; // fail quietly; should never happen
       }
 
-      // Find the associated node
+      // Find the associated node and open a window for it
       Enumeration<TreeNode> bfe = operatorWindow.breadthFirstEnumeration();
+      boolean applied = false;
       while (bfe.hasMoreElements()) {
         OperatorNode node = (OperatorNode) bfe.nextElement();
         if (node.getFileName() == null) continue; // skip the fluff
@@ -63,7 +67,9 @@ public class Cfg {
         // calculate the canonical path so equality comparison will work as intended
         String nodeFN;
         try {
-          nodeFN = (new File(node.getFileName())).getCanonicalPath();
+          nodeFN = projectRoot.resolve(node.getFileName()).toRealPath().toString();
+//          TODO next: relativize to cfgFile
+//          nodeFN = (new File(node.getFileName())).getCanonicalPath();
         } catch (IOException ioe) {
           continue; // fail quietly; should never happen
         }
@@ -71,10 +77,17 @@ public class Cfg {
         if (nodeFN.equals(target)) {
           if (tokens[0].equals("RULEEDITOR")) {
             node.openRules(mainFrame);
+            applied = true;
           } else if (tokens[0].equals("DATAMAP")) {
             node.openDataMap(operatorWindow.getDatamap(), mainFrame);
+            applied = true;
+          } else {
+            LOGGER.warning("Unknown directive in cfg file: " + tokens[0]);
           }
         }
+      }
+      if (!applied) {
+        LOGGER.warning("Unable to apply line from cfg file: " + line);
       }
     }
   }
@@ -90,7 +103,15 @@ public class Cfg {
    */
   public static void writeCfgFile(MainFrame mainFrame) {
     // Sanity check:  is a project currently open?
-    if (mainFrame.getOperatorWindow() == null) return;
+    if (mainFrame.getOperatorWindow() == null) {
+      return;
+    }
+
+    Path cfgFile = getCfgFile(mainFrame.getOperatorWindow());
+    if (cfgFile == null) {
+      return;
+    }
+    Path projectRoot = cfgFile.getParent();
 
     // Create the .cfg file contents
     ArrayList<String> cfgLines = new ArrayList<>();
@@ -98,7 +119,7 @@ public class Cfg {
     for (JInternalFrame jInternalFrame : jif) {
       if (jInternalFrame instanceof RuleEditor) {
         RuleEditor re = (RuleEditor) jInternalFrame;
-        String line = "RULEEDITOR " + re.getFile();
+        String line = "RULEEDITOR " + getPathForWriting(projectRoot, re.getFile());
         cfgLines.add(line);
       } else if (jInternalFrame instanceof DataMap) {
         DataMap dm = (DataMap) jInternalFrame;
@@ -111,7 +132,7 @@ public class Cfg {
           if (node instanceof SoarOperatorNode) {
             SoarOperatorNode son = (SoarOperatorNode) node;
             if (son.getDataMapIdNumber() == dmId) {
-              String line = "DATAMAP " + son.getFileName();
+              String line = "DATAMAP " + getPathForWriting(projectRoot, son.getFileName());
               cfgLines.add(line);
               break;
             }
@@ -121,32 +142,41 @@ public class Cfg {
     }
 
     // Write the .cfg file contents
-    File cfgFile = getCfgFile(mainFrame.getOperatorWindow());
-    if ((cfgFile != null) && (cfgFile.exists())) {
-      try {
-        PrintWriter pw = new PrintWriter(cfgFile);
-        for (String line : cfgLines) {
-          pw.println(line);
-        }
-        pw.close();
-      } catch (FileNotFoundException fnfe) {
-        // the .cfg file is not essential so just report it if it can't be written
-        mainFrame.getFeedbackManager().setStatusBarMsg("Unable to save current configuration to " + cfgFile.getName());
-      }
+    try {
+      saveToFile(
+          cfgFile,
+          (OutputStream out) -> {
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
+              for (String line : cfgLines) {
+                writer.write(line);
+                writer.write("\n");
+              }
+            }
+          });
+    } catch (IOException e) {
+      // the .cfg file is not essential so just report it if it can't be written
+      mainFrame
+          .getFeedbackManager()
+          .setStatusBarMsg("Unable to save current configuration to " + cfgFile.getFileName());
     }
+  }
+
+  private static String getPathForWriting(Path projectRoot, String fileName) {
+    // write path relative to project root, and use / instead of \ for cross-platform support
+    return projectRoot.relativize(Paths.get(fileName)).toString().replace("\\", "/");
   }
 
   /**
    * @return a File object representing where the project's .cfg file should be
    */
-  private static File getCfgFile(OperatorWindow operatorWindow) {
+  private static Path getCfgFile(OperatorWindow operatorWindow) {
     Object root = operatorWindow.getModel().getRoot();
     if (root instanceof OperatorRootNode) {
       OperatorRootNode orn = (OperatorRootNode) root;
-      String cfgFN = orn.getFolderName() + File.separator + orn.getName() + ".cfg";
-      return new File(cfgFN);
+      Path projectDir = Paths.get(orn.getFolderName());
+      return projectDir.resolve(orn.getName() + ".cfg");
     }
-
+    LOGGER.warning("Attempted to get .cfg path when root was not an operator window");
     return null;
   }
 }
